889bb6c61e6b3ec1a99cbcc8611ba58ef2e588ca
liushengming <lsmya@qq.com>
2024-06-14 15:53:10 +0800

    修改json拓展方法

--- smart/src/main/java/cn/lsmya/smart/extension/JSON.kt
+++ smart/src/main/java/cn/lsmya/smart/extension/JSON.kt
@@ -6,23 +6,29 @@ import com.squareup.moshi.JsonAdapter.Factory
 import java.io.IOException
 import java.lang.reflect.Type
 
-private val moshi: Moshi by lazy {
-    Moshi.Builder()
-        .add(NullToEmptyStringAdapter)
-        .add(MoshiArrayListJsonAdapter.FACTORY)
-        .build()
-}
+val moshi = Moshi.Builder()
+    .add(Null_To_Empty_String_Adapter)
+    .add(Null_To_INT_0_Adapter)
+    .add(Null_To_LONG_0_Adapter)
+    .add(Null_To_False_Adapter)
+    .add(MoshiArrayListJsonAdapter.FACTORY)
+    .build()
 
-fun Any.toJson(): String = Gson().toJson(this)
+fun Any.toJsonByGson(): String = Gson().toJson(this)
+fun Any.toJson(): String = moshi.adapter(this.javaClass).toJson(this)
 
 fun <T> String.fromJson(classOfT: Class<T>): T = moshi.adapter(classOfT).fromJson(this)!!
 
 fun <T> String.fromJson(type: Type): T = moshi.adapter<T>(type).fromJson(this)!!
-
 fun <T> String.fromJsonList(classOfT: Class<T>): ArrayList<T> =
-    this.fromJson(Types.newParameterizedType(ArrayList::class.java, classOfT))
+    getClassType(classOfT).fromJson(this)!!
 
-private object NullToEmptyStringAdapter {
+private fun <T> getClassType(classOfT: Class<T>): JsonAdapter<ArrayList<T>> {
+    val type: Type = Types.newParameterizedType(ArrayList::class.java, classOfT)
+    return moshi.adapter(type)
+}
+
+object Null_To_Empty_String_Adapter {
     @FromJson
     fun fromJson(reader: JsonReader?): String {
         if (reader == null) {
@@ -37,10 +43,54 @@ private object NullToEmptyStringAdapter {
 
 }
 
+object Null_To_False_Adapter {
+    @FromJson
+    fun fromJson(reader: JsonReader?): Boolean {
+        if (reader == null) {
+            return false
+        }
+        if (reader.peek() != JsonReader.Token.NULL) {
+            return reader.nextBoolean()
+        }
+        reader.nextNull<Unit>()
+        return false
+    }
+
+}
+
+object Null_To_INT_0_Adapter {
+    @FromJson
+    fun fromJson(reader: JsonReader?): Int {
+        if (reader == null) {
+            return 0
+        }
+        if (reader.peek() != JsonReader.Token.NULL) {
+            return reader.nextInt()
+        }
+        reader.nextNull<Unit>()
+        return 0
+    }
+
+}
+object Null_To_LONG_0_Adapter {
+    @FromJson
+    fun fromJson(reader: JsonReader?): Long {
+        if (reader == null) {
+            return 0L
+        }
+        if (reader.peek() != JsonReader.Token.NULL) {
+            return reader.nextLong()
+        }
+        reader.nextNull<Unit>()
+        return 0L
+    }
+
+}
+
 private abstract class MoshiArrayListJsonAdapter<C : MutableCollection<T>?, T> private constructor(
     private val elementAdapter: JsonAdapter<T>
 ) :
-    JsonAdapter<C>() {
+    com.squareup.moshi.JsonAdapter<C>() {
     abstract fun newCollection(): C
 
     @Throws(IOException::class)
@@ -69,7 +119,7 @@ private abstract class MoshiArrayListJsonAdapter<C : MutableCollection<T>?, T> p
 
     companion object {
         val FACTORY = Factory { type, annotations, moshi ->
-            val rawType = Types.getRawType(type)
+            val rawType = com.squareup.moshi.Types.getRawType(type)
             if (annotations.isNotEmpty()) return@Factory null
             if (rawType == ArrayList::class.java) {
                 return@Factory newArrayListAdapter<Any>(
