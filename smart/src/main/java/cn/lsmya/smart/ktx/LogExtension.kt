23c94d8cf67fd0a96e04e2bd5dc3a08b542aaf5a
liushengming <lsmya@qq.com>
2024-11-20 14:21:39 +0800

    增加超长日志打印

--- smart/src/main/java/cn/lsmya/smart/ktx/LogExtension.kt
+++ smart/src/main/java/cn/lsmya/smart/ktx/LogExtension.kt
@@ -1,60 +1,111 @@
 package cn.lsmya.smart.ktx
 
+import android.content.Intent
 import android.util.Log
 
-fun Any?.eJson() {
-    this?.let {
-        log(Log.ERROR, it.toJson())
-    }
+fun Any?.d() {
+    log(Log.DEBUG, null, this, 4)
 }
 
 fun Any?.e() {
-    log(Log.ERROR, this)
+    log(Log.ERROR, null, this, 4)
 }
 
-fun logd(any: Any?) {
-    log(Log.DEBUG, any)
+fun Any?.i() {
+    log(Log.INFO, null, this, 4)
 }
 
-fun loge(any: Any?) {
-    log(Log.ERROR, any)
+fun Any?.v() {
+    log(Log.VERBOSE, null, this, 4)
 }
 
-fun logi(any: Any?) {
-    log(Log.INFO, any)
+fun Any?.w() {
+    log(Log.WARN, null, this, 4)
 }
 
-fun logv(any: Any?) {
-    log(Log.VERBOSE, any)
+fun logd(any: Any?, tag: String?) {
+    log(Log.DEBUG, tag, any, 5)
 }
 
-fun logw(any: Any?) {
-    log(Log.WARN, any)
+fun loge(any: Any?, tag: String? = null) {
+    log(Log.ERROR, tag, any, 5)
 }
 
-private fun log(method: Int, any: Any?) {
+fun logi(any: Any?, tag: String?) {
+    log(Log.INFO, tag, any, 5)
+}
+
+fun logv(any: Any?, tag: String?) {
+    log(Log.VERBOSE, tag, any, 5)
+}
+
+fun logw(any: Any?, tag: String?) {
+    log(Log.WARN, tag, any, 5)
+}
+
+private fun log(method: Int, tag: String?, any: Any?, stackTraceIndex: Int) {
     any?.let {
-        val logMsg =
-            if (it is String || it is Int || it is Double || it is Float || it is Boolean) {
-                it.toString()
-            } else {
-                it.toJson()
+        val mTag = tag.ifNullOrEmpty { "tag" }
+        var logMsg = getLogMessage(it)
+        if (logMsg.isNotNullOrEmpty()) {
+            val stackTrace = Thread.currentThread().stackTrace[stackTraceIndex]
+            val fileName = stackTrace.fileName
+            val lineNumber = stackTrace.lineNumber
+            val methodName = stackTrace.methodName
+            val msgHead = buildString {
+                append("\n╔====================(")
+                append(fileName)
+                append(":")
+                append(lineNumber)
+                append(")#")
+                append(methodName)
+                append("=============================================================================")
             }
-        val stackTrace = Thread.currentThread().stackTrace[4]
-        val tag = "log"
-        val msg = String.format(
-            "(%s:%d)#%s: ",
-            stackTrace.fileName,
-            stackTrace.lineNumber,
-            stackTrace.methodName
-        ) + logMsg
-        when (method) {
-            Log.DEBUG -> Log.d(tag, msg)
-            Log.ERROR -> Log.e(tag, msg)
-            Log.INFO -> Log.i(tag, msg)
-            Log.VERBOSE -> Log.v(tag, msg)
-            Log.WARN -> Log.w(tag, msg)
-            else -> Log.wtf(tag, msg)
+            print(method, mTag, msgHead)
+            val maxStrLength = 2001 - mTag.length
+            while (logMsg!!.length > maxStrLength) {
+                print(method, mTag, logMsg.substring(0, maxStrLength))
+                logMsg = logMsg.substring(maxStrLength);
+            }
+            print(method, mTag, logMsg)
+            val foot = buildString {
+                append("╚====================(")
+                append(fileName)
+                append(":")
+                append(lineNumber)
+                append(")#")
+                append(methodName)
+                append("=============================================================================\n")
+            }
+            print(method, mTag, foot)
         }
+    }
+}
+
+private fun print(method: Int, tag: String, msg: String) {
+    when (method) {
+        Log.DEBUG -> Log.d(tag, msg)
+        Log.ERROR -> Log.e(tag, msg)
+        Log.INFO -> Log.i(tag, msg)
+        Log.VERBOSE -> Log.v(tag, msg)
+        Log.WARN -> Log.w(tag, msg)
+        else -> Log.wtf(tag, msg)
+    }
+}
+
+private fun getLogMessage(any: Any): String? {
+    return if (any is String || any is Int || any is Double || any is Float || any is Boolean) {
+        any.toString()
+    } else if (any is Intent) {
+        val extras = any.extras
+        if (extras != null) {
+            val map = HashMap<String, Any?>()
+            extras.keySet().map { map[it] = extras.get(it) }
+            return map.toJsonByGson()
+        } else {
+            return null;
+        }
+    } else {
+        any.toJson()
     }
 }
